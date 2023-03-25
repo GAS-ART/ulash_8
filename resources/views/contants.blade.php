6151fefddcc78e033b19f8dc1da72e70b0755ba0
Rozhkov Mykyta <rozhkov.mykyta@gmail.com>
2023-03-25 16:23:33 +0200

    Added a contact page

--- resources/views/contants.blade.php
+++ resources/views/contants.blade.php
@@ -9,7 +9,58 @@
 
 
 @section('content')
-<h1>@lang('service.title')4</h1>
-<p>@lang('service.text')</p>
-<a href="{{ route('home', ['locale' => __('lang.current')]) }}" style="color: darkblue; text-decoration: underline">@lang('service.link')</a>
+<!-- <div id="map"></div> -->
+<iframe id="myMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3536.521602676732!2d-85.78990368496162!3d38.271750979690235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8869e0af648051a7%3A0x67c7df546d0e8e8c!2sChurchill%20Downs!5e0!3m2!1sen!2sus!4v1616189963388!5m2!1sen!2sus" width="100%" height="660" style="border:0;" allowfullscreen="" loading="lazy" style="filter: grayscale(100%);" frameborder="0"></iframe>
+
+<div class="contacts__container contacts">
+  <div class="contacts__wrapper">
+    <div class="contacts__content">
+      <div class="popup__text">
+        <h3 class="popup__title-h3 section-name">связаться с нами</h3>
+        <h2 class="popup__title-h2 section-title">Контакты</h2>
+        <ul class="footer__menu-list">
+          <li class="footer__menu-item">
+            <a class="footer__contacts-item" href="#">
+              <span class="footer__contacts-icon icon-point"></span>
+              <p class="footer__contacts-address">Barselona, adress adress</p>
+            </a>
+          </li>
+          <li class="footer__menu-item">
+            <a class="footer__contacts-item" href="tel:+34617051264">
+              <span class="footer__contacts-icon icon-phone"></span>
+              <p class="footer__contacts-phone">+34 617-05-12-64</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <form class="popup__form @lang('lang.current') page" action="#" method="post">
+        @csrf
+        <div class="popup__form-block">
+          <div class="popup__form-box">
+            <div>
+              <input class="popup__name" placeholder="Ваше имя*" tabindex="1" name="name" type="text" />
+              <p class="name-error"></p>
+            </div>
+            <div class="contacts__phon-mail">
+              <div>
+                <input class="popup__phone" placeholder="Номер телефона*" tabindex="2" name="phone" />
+                <p class="phone-error"></p>
+              </div>
+              <input class="popup__email" placeholder="Email" tabindex="3" name="email" type="text" />
+            </div>
+            <div>
+              <textarea class="popup__textarea" placeholder="Комментарий" name="text" tabindex="5"></textarea>
+            </div>
+          </div>
+        </div>
+
+        <div class="popup__button-wrapper">
+          <button class="popup__button" type="submit">Оставить заявку</button>
+        </div>
+
+      </form>
+    </div>
+  </div>
+</div>
+
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="@lang('lang.current')">
 @include('inc.head')
+
 <body id="body">
-   <div class="wrapper">
-      @include('inc.header')
-         <main class="page">
-            @yield('content')
-         </main>
-      @include('inc.footer')
-   </div>
-   @include('inc.popup')
-   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-   <script src="{{asset('js/app.js') . '?_v=2'}} "></script>
+  <div class="wrapper">
+    @include('inc.header')
+    <main class="page">
+      @yield('content')
+    </main>
+    @include('inc.footer')
+  </div>
+  @include('inc.popup')
+  <!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBxkxUV5WWDX7oBj5U84uz0OczhiVwUTIs"></script> -->
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+  <script src="{{asset('js/app.js') . '?_v=2'}} "></script>
 </body>
 
 </html>
